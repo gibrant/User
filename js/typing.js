60e33a684b0850f43c6a97d7760712fb0b84e811
gibrant <gibrant49@gmail.com>
2018-12-02 15:11:36 +0700

    Github Pages V3

---
+++ js/typing.js
@@ -0,0 +1,9 @@
+var typed3 = new Typed('.typed', {
+    strings: ['<b>Designer</b>', '<b>Developer</b>', '<b>Freelancer</b>', '<b>Artist</b>'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 500,
+    startDelay: 1000,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
